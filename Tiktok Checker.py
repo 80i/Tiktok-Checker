f7dec16948fc668041d181b2abc1e0a47874425b
K4Sp3r <77979875+80i@users.noreply.github.com>
2021-01-30 19:12:52 +0300

    Create Tiktok Checker.py

---
+++ Tiktok Checker.py
@@ -0,0 +1,53 @@
+import random, string, requests, time
+from colorama import Fore
+from discord_webhook import DiscordWebhook
+import os
+os.system("cls" or "clear")
+print(Fore.RED+"""
+
+Discord Server :  https://discord.gg/eJeHG7D6pm
+        My discord : @K4Sp3r#1337
+
+ _________  _   __      _________       __              ______  __                    __                     
+|  _   _  |(_) [  |  _ |  _   _  |     [  |  _        .' ___  |[  |                  [  |  _                 
+|_/ | | \_|__   | | / ]|_/ | | \_|.--.  | | / ]      / .'   \_| | |--.  .---.  .---.  | | / ] .---.  _ .--.  
+    | |   [  |  | '' <     | |  / .'`\ \| '' <       | |        | .-. |/ /__\\/ /'`\] | '' < / /__\\[ `/'`\] 
+   _| |_   | |  | |`\ \   _| |_ | \__. || |`\ \      \ `.___.'\ | | | || \__.,| \__.  | |`\ \| \__., | |     
+  |_____| [___][__|  \_] |_____| '.__.'[__|  \_]      `.____ .'[___]|__]'.__.''.___.'[__|  \_]'.__.'[___]    
+                                                                                                             
+
+    Made By : K4Sp3r
+     Insta : @K4S 
+      Snap : Fuun
+"""+Fore.WHITE)
+
+time.sleep(.5)
+
+hmletters = int(input('how many letters you want to check : '))
+print("")
+#wbhook = 'https://discord.com/api/webhooks/803917756138782732/UJoDveWMHPeWNQHAovOfM4MUjsv4N9gKWmFHpoW7_S7NyEDl1JNp-jurZF48NG1eo7MI'
+print()
+time.sleep(.3)
+
+
+while True:
+    usernames = ('').join(random.choices(string.ascii_lowercase + string.digits, k=hmletters))
+    #webhook = DiscordWebhook(url=wbhook, content=f'`{usernames}` | Might be Available or Banned on => ||`{webss}`|| |')
+    r = requests.get(f'https://m.tiktok.com/node/share/user/@{usernames}')
+    responsecode = r.json()['statusCode']
+    
+    if responsecode == 10202:
+        print(Fore.CYAN + "[+] " + Fore.GREEN + "Available" + Fore.WHITE + ' |=>' + Fore.LIGHTMAGENTA_EX + f' {usernames}'+Fore.WHITE+" <=|" + Fore.CYAN + " [+]")
+        f = open("availables.txt", "a", encoding='utf-8')
+        f.write(f"{usernames} | Might be Available or Banned|\n")
+        #webhook.execute()
+    elif responsecode == 10222:
+        print(Fore.CYAN+"[-] "+Fore.RED + "UnAvailable"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
+    elif responsecode == 0:
+        print(Fore.CYAN+"[-] "+Fore.RED + "UnAvailable"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
+    elif responsecode == 10221:
+        print(Fore.CYAN+"[-] "+Fore.RED + "Banned"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
+    elif responsecode == 10223:
+        print(Fore.CYAN+"[-] "+Fore.RED + "Banned"+ Fore.WHITE +' |=>'+Fore.YELLOW+ f' {usernames}'+Fore.WHITE+" <=|"+Fore.CYAN+" [-]")
+    else:
+        print(Fore.CYAN+"[X] "+Fore.WHITE+f"i cant find {usernames}'s statusCode !!' "+Fore.CYAN+" [X]"+Fore.WHITE)
